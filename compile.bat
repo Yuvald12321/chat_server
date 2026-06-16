78524feb56a8036ccdb61172836a352617609fd6
Yuval <yuvalivshits@gmail.com>
2026-06-16 19:45:51 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
-pyinstaller --noconsole --onefile --icon logo.ico --add-data "templates/index.html;templates" -n "Local Chat Server" main.py
+pyinstaller --noconsole --noconfirm --onefile --icon logo.ico --add-data "templates/index.html;templates" -n "Local Chat Server" main.py
 rd /S /Q "build"
-del /Q "Local Chat Server.spec"
