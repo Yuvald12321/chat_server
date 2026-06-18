7afba341eb1270abe2d2c8a94f3c727a1e38e56c
Yuval <yuvalivshits@gmail.com>
2026-06-18 12:25:44 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-pyinstaller --noconsole --noconfirm --onefile --icon logo.ico --add-data "templates/index.html;templates" -n "Local Chat Server" main.py
+call ".venv\Scripts\pyinstaller.exe" --specpath "build" --onefile --noconsole --icon "logo.ico" --add-data "../templates/index.html;templates" -n "Local Chat Server" main.py
 rd /S /Q "build"
